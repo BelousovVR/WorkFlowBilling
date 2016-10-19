8c135c7ca9abd11346c33fa33d8fd113030a9a4f
Belousov V.R. <paladin05@mail.ru>
2016-10-19 17:25:27 +0400

    Created new project

---
+++ WorkFlowBilling/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WorkFlowBilling.MvcApplication" Language="C#" %>
---
+++ WorkFlowBilling/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkFlowBilling.Startup))]
+namespace WorkFlowBilling
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
