e49f5dc7c64578ba07baddcd39adf82fde7e7331
Belousov V.R. <paladin05@mail.ru>
2016-10-20 09:29:34 +0400

    Update jquery / bootstrap

--- WorkFlowBilling/App_Start/BundleConfig.cs
+++ WorkFlowBilling/App_Start/BundleConfig.cs
@@ -9,10 +9,8 @@ namespace WorkFlowBilling
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery/jquery-{version}.js",
+                        "~/Scripts/jquery/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -20,11 +18,12 @@ namespace WorkFlowBilling
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap/bootstrap.js",
+                      "~/Scripts/bootstrap/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/Bootstrap/bootstrap-theme.css",
+                      "~/Content/Bootstrap/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
