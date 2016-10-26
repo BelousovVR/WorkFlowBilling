f8512af1c46f3ee8f6882cd2eb3e8513a999e45a
Belousov V.R. <paladin05@mail.ru>
2016-10-26 17:25:15 +0400

    Add ContainerManagerTests

---
+++ WorkFlowBilling.IoC.Tests/Container/ContainerManagerTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using WorkFlowBilling.IoC.Container;
+using System.Web.Mvc;
+using Autofac.Integration.Mvc;
+
+namespace WorkFlowBilling.IoC.Tests.Extensions
+{
+    /// <summary>
+    /// Тесты IoC контейнера
+    /// </summary>
+    [TestFixture]
+    public class ContainerManagerTests
+    {
+        [Test]
+        public void ContainerManager_DependencyResolver_SetCurrent()
+        {
+            //GIVEN
+            var containerManager = new ContainerManager();
+          
+            //WHEN
+            containerManager.SetAspNetMvcResolver();
+
+            //THEN
+            Assert.IsTrue(DependencyResolver.Current is AutofacDependencyResolver);
+        }
+    }
+}
